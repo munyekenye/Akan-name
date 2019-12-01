741bbd4b7804172890f54d26784374e48066642f
munyekenye <munyekenyelilian@gmail.com>
2019-12-01 15:11:49 +0300

    added vaidation

--- js/script.js
+++ js/script.js
@@ -36,3 +36,17 @@ var maleNames=[
      Var male= document.getElementById ("male");
      Var female = document.getElementById ("female");
     }
+// validation
+if(day<=0 || day>31)
+alert ("Please enter date of birth");
+else if (month<0|| month>12)
+alert ("Please enter month of birth");
+var day =new Date(Year + "/"+ month + "/"+ day);
+var birthDay=getDay();
+if(male.checked==true){
+    alert ("Your date of birth") + daysOfWeek[birthDay]+ "Therefore your Akan Name is" + maleNames [birthDay])
+}
+if(female.checked==true){
+    alert ("Your date of birth") + daysOfWeek[birthDay]+ "Therefore your Akan Name is" + femaleNames [birthDay])
+}
+}
