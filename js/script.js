ef3a23684c1625f1b8a969a5c10a3e61e5fa334b
munyekenye <munyekenyelilian@gmail.com>
2019-12-02 08:08:50 +0300

    correction of grammar

--- js/script.js
+++ js/script.js
@@ -29,25 +29,26 @@ var maleNames=[
  ];
  // calling functions
  function getName (){
-     var year=parseInt(document.getElementById("Year").value);
-     var Month=parseInt(document.getElementById("Month").value);
-     var Day =parseInt(document.getElementById("Day").value);
+     var year=parseInt(document.getElementById("year").value);
+     var month=parseInt(document.getElementById("month").value);
+     var day =parseInt(document.getElementById("day").value);
      var male= document.getElementById ("male");
      var female = document.getElementById ("female");
-    }
+    
 // validation
 if(day<=0 || day>31)
 alert ("Please enter date of birth");
 else if (month<0|| month>12)
 alert ("Please enter month of birth");
 var day =new Date(Year + "/"+ month + "/"+ day);
-var birthDay=getDay();
+var birthDay= day.getDay();
 
 if(male.checked==true){
-    alert ("Your date of birth") + daysOfWeek[birthDay]+ "Therefore your Akan Name is" + maleNames [birthDay])
+    alert ("Your date of birth") + daysOfWeek[birthDay]+ "Therefore your Akan Name is" + maleNames[birthdDay]
 }
-{
+
 else if(female.checked==true){
-    alert ("Your date of birth") + daysOfWeek[birthDay]+ "Therefore your Akan Name is" + femaleNames [birthDay])
+    alert ("Your date of birth") + daysOfWeek[birthDay]+ "Therefore your Akan Name is" + femaleNames [birthDay]
 }
+
 }
